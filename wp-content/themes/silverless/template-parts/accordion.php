b9d7b3e1dff57fb0bc05557bc0431a0f5265c1f6
Andy Paddock <appaddock@gmail.com>
2024-11-15 15:42:08 +0000

    back

--- wp-content/themes/silverless/template-parts/accordion.php
+++ wp-content/themes/silverless/template-parts/accordion.php
@@ -7,7 +7,7 @@ global $sidebar_content;
 ?>
 
 <section
-  class="container section-accordion spacer spacer__<?php echo $space_size; ?> <?php echo $sidebar_content ? "sidebar__content" : ""; ?>">
+  class="container section-accordion spacer spacer__<?php echo $space_size; ?> <?php echo $sidebar_content ? "sidebar__content" : ""; ?> <?php echo esc_html ( get_sub_field('background_colour') ); ?>">
   <div class="row__10">
     <?php if (have_rows('records')) { ?>
       <div class="accordion">
@@ -19,7 +19,7 @@ global $sidebar_content;
           ?>
           <div class="accordion__record">
             <div class="accordion__heading">
-              <h3 class=" heading heading__lg font__color__primary">
+              <h3 class=" heading heading__lg ">
                 <?php echo $title; ?>
               </h3>
               <?php echo get_template_part('/inc/img/arrow'); ?>
