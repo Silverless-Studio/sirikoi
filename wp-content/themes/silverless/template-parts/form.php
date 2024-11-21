39bea5a9419770ea5a6acedc36f4d02afe15aad7
Andy Paddock <appaddock@gmail.com>
2024-11-21 12:37:29 +0000

    contact form

--- wp-content/themes/silverless/template-parts/form.php
+++ wp-content/themes/silverless/template-parts/form.php
@@ -6,37 +6,77 @@ $container_width = get_sub_field('container_width');
 $override_container_offset = get_sub_field('override_container_offset');
 $container_offset = get_sub_field('container_offset');
 
-$space_size = get_sub_field('space_size');
+$contact_email_address = get_field('contact_email_address', 'options');
+$contact_address = get_field('contact_address', 'options');
+$what_three_words = get_field('what_three_words', 'options');
+$google_maps_link = get_field('google_maps_link', 'options');
 
-$style = "";
-if ($override_container_offset) {
-  $style = "--container-offset: " . $container_offset . ";";
-}
+$space_size = get_sub_field('space_size');
 
 
 global $sidebar_content;
 
 ?>
-
+<?php if(get_sub_field('include_contact')):?>
 
 <section
-  class="container section-form spacer spacer__<?php echo $space_size; ?> <?php echo $sidebar_content ? "sidebar__content" : ""; ?>">
-  <div class="row__<?php echo $container_width; ?>" style="<?php echo $style; ?>">
-    <?php if ($style_form_container) { ?>
-      <div class="testimonials">
-        <img src="/wp-content/themes/silverless/inc/img/stars.png" alt="" class="testimonials__stars__outside" />
-        <div class="testimonials__wrapper">
-          <img src="/wp-content/themes/silverless/inc/img/stars.png" alt="" class="testimonials__stars__inside" />
-          <div class="grid grid__10">
-            <div class="row__10">
-              <?php echo do_shortcode($form_shortcode); ?>
-            </div>
-          </div>
+  class="container section-form section-contact-details spacer spacer__<?php echo $space_size; ?> <?php echo $sidebar_content ? "sidebar__content" : ""; ?>">
+  <div class="row grid grid__12 contact-details">
+    <div class="col-offset__2 col-span__3 contact-details__details paragraph small">
+
+      <h2 class="heading heading__lg">Contact Us</h2>
+      <?php if (have_rows('contact_phone_numbers', 'option')) { ?>
+        <div>
+          <?php while (have_rows('contact_phone_numbers', 'option')) { ?>
+            <?php
+            the_row();
+            $contact_phone_number = get_sub_field('contact_phone_number');
+            ?>
+            <a href="tel://<?php echo $contact_phone_number; ?>">
+              <?php echo $contact_phone_number; ?>
+            </a>
+          <?php } ?>
         </div>
-      </div>
-    <?php } else { ?>
-      <?php get_template_part('template-parts/additional-artwork'); ?>
+      <?php } ?>
+      <?php if ($contact_email_address) { ?>
+        <a href="mailto://<?php echo $contact_email_address; ?>">
+          <?php echo $contact_email_address; ?>
+        </a>
+      <?php } ?>
+      <?php if ($contact_address) { ?>
+        <p class="small">
+          <?php echo $contact_address; ?>
+        </p>
+      <?php } ?>
+      <?php if ($what_three_words || $google_maps_link) { ?>
+        <div>
+          <?php if ($what_three_words) { ?>
+            <a href="<?php echo $what_three_words['url']; ?>" target="<?php echo $what_three_words['target']; ?>">
+              <?php echo $what_three_words['title']; ?>
+            </a>
+          <?php } ?>
+          <?php if ($google_maps_link) { ?>
+            <a href="<?php echo $google_maps_link['url']; ?>" target="<?php echo $google_maps_link['target']; ?>">
+              <?php echo $google_maps_link['title']; ?>
+            </a>
+          <?php } ?>
+        </div>
+      <?php } ?>
+    </div>
+    <div class="col-span__8 contact-details__map">
       <?php echo do_shortcode($form_shortcode); ?>
-    <?php } ?>
+    </div>
+     
+
   </div>
 </section>
+<?php else:?>
+  <section
+  class="container section-form spacer spacer__<?php echo $space_size; ?>">
+  <div class="row">
+    
+      <?php echo do_shortcode($form_shortcode); ?>
+
+  </div>
+</section>
+  <?php endif;?>
